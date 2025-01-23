a0363a98d7fd5801f31748147e2d1370159007ca
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2025-01-23 15:31:46 -0800

    change build system, add ruff config, add doc

---
+++ docs/source/conf.py
@@ -0,0 +1,16 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from sphinx_pyproject import SphinxConfig
+
+# -- Project information -----------------------------------------------------
+# we are using the pyproject.toml file to store the project metadata
+# see https://github.com/sphinx-toolbox/sphinx-pyproject
+config = SphinxConfig("../../pyproject.toml", globalns=globals())
+
+
+templates_path = ['_templates']
+
+
+
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. NUGETS documentation master file, created by
+   sphinx-quickstart on Thu Jan 23 14:11:06 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+NUGETS documentation
+====================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
