c37a0f273a6df3bfdab6009add37ec85fcfdfeb6
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2025-01-26 22:13:06 -0800

    some documentation and dependencies

---
+++ nugets/__init__.py
@@ -0,0 +1,22 @@
+"""NUGETS — NeUral GEomeTry benchmarkS
+
+NUGETS is a benchmark suite for neural geometry processing tasks. 
+
+It provides a comprehensive set of datasets and tasks for evaluating the performance of 
+neural networks on geometry tasks.
+
+
+Methodology: 
+
+    1. Datasets:
+        We provide a set of synthetic and real-world datasets for evaluating neural 
+        networks on geometry tasks.
+
+    2. Tasks:
+        For each dataset, we provide a set of tasks that can be used to evaluate the
+        performance of neural networks on geometry tasks.
+
+    3. Metrics:
+
+
+"""
