79abd78beaaa41a90b3b1e0537176fc35c71f9ce
chens5 <srchen98@gmail.com>
2025-06-05 16:16:59 -0700

    updated sumformer with backbone aggregation fn

--- nugets/models/backbones/sumformer.py
+++ nugets/models/backbones/sumformer.py
@@ -1,6 +1,7 @@
 from torch_heterogeneous_batching.nn.sumformer import Sumformer as Sumformer_nn
+from torch_geometric.nn.resolver import aggregation_resolver
 
-from nugets.models.backbone import BackBone, int_hyperparameter, model_attribute
+from nugets.models.backbone import BackBone, int_hyperparameter,hyperparameter, model_attribute
 from nugets.models.backbones.register import register
 
 @register
@@ -12,6 +13,8 @@ class Sumformer(BackBone):
     d_model: int = int_hyperparameter(description="number of dimensions of the input and the output")
 
     feed_forward_hidden_dim: int = int_hyperparameter(description="hidden dimension")
+
+    aggregation: str = hyperparameter(type=str, description="sequence aggregation function")
     
     sumformer: Sumformer_nn = model_attribute()
 
@@ -21,9 +24,14 @@ class Sumformer(BackBone):
             hidden_dim = self.feed_forward_hidden_dim,
             num_blocks=self.n_layers
         )
+        aggregation_args = {}
+        if self.aggregation != "none":
+            self.aggregation_fn = aggregation_resolver(self.aggregation, **aggregation_args)
     
     def forward(self, batch, return_reg_loss=False):
         del return_reg_loss # no regularization loss for SumFormers
+        if self.aggregation != "none":
+            return self.aggregation_fn(self.sumformer(batch).data, ptr=batch.ptr), None
         return self.sumformer(batch), None
     
     def get_input_dim(self): return self.d_model
--- tests/test_models.py
+++ tests/test_models.py
@@ -13,8 +13,8 @@ from pathlib import Path
 
 def test_transformer_with_labeled_set_task():
     """
-    Tests training on labelled tasks as well as aggregation step from the BackBone
-    TODO: throw device-side assert when 'max' aggregation is used.  
+    Tests training on labelled tasks as well as aggregation step from the transformer BackBone
+    TODO: throws a runtime error (device-side assert) when 'max' aggregation is used.  
     """
     assert issubclass(Model, pl.LightningModule)
 
@@ -55,6 +55,47 @@ def test_transformer_with_labeled_set_task():
                          **additional_options)
 
     trainer.fit(model=model)
+
+def test_sumformer_with_labeled_set_task():
+    """
+    tests training on labelled tasks with SumFormer backbone
+    """
+    assert issubclass(Model, pl.LightningModule)
+
+    path = Path('/home/sam/NUGETs/config.yaml')
+    Config.load(path)
+    global_config = Config.get()
+    task = SingleLabelDummyTask(
+            "GrowingCircles", 
+            dict(
+                dim=2,
+                min_points=10,
+                max_points=20,
+                radius="linear", 
+                length= 64, 
+                )
+            )
+    backbone: BackBone = Sumformer(
+        n_layers = 2,
+        d_model=64,
+        feed_forward_hidden_dim=256,
+        aggregation='sum'
+    )
+    model = Model(backbone, task, batch_size=32, learning_rate=1e-3, debug_mode=True)
+    additional_options = dict()
+    trainer = pl.Trainer(default_root_dir="workdir/", 
+                         max_epochs=1, 
+                         limit_train_batches=2,
+                         log_every_n_steps=1,
+                         gradient_clip_val=0, 
+                        #  precision="16-mixed",
+                         logger=False, 
+                         use_distributed_sampler=False,
+                         devices=1,
+                         **additional_options)
+
+    trainer.fit(model=model)
+    
 
 def test_transformer_with_dummy_set_to_set_task():
 
@@ -120,7 +161,8 @@ def test_sumformer_with_dummy_set_to_set_task():
     backbone: BackBone = Sumformer(
         n_layers = 2,
         d_model=64,
-        feed_forward_hidden_dim=256
+        feed_forward_hidden_dim=256,
+        aggregation='none'
     )
 
     model = Model(backbone, task, batch_size=32, learning_rate=1e-3, debug_mode=True)
