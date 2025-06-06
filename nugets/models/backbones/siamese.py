fixed mixed precision

nugets/models/backbones/siamese.py
@@ -11,6 +11,8 @@ from nugets.models.backbone import (BackBone, int_hyperparameter, bool_hyperpara
 from nugets.models.backbones.register import register
 import nugets.losses.losses as Losses
 
+from torch_geometric.nn.resolver import aggregation_resolver
+
 
 @register
 class CoupledNetwork(BackBone):
@@ -38,6 +40,9 @@ class CoupledNetwork(BackBone):
 
     latent_dimension: int = int_hyperparameter(description="dimension of the latent space, set it to 0 to disable latent")
     p: int = int_hyperparameter(description="L_p distance function")
+
+    aggregation: str = hyperparameter(default='mean', type=str, description="aggregation function")
+
     decoder_distance: str = hyperparameter(type=str)
 
     encoder1: BackBone = model_attribute()
@@ -73,16 +78,23 @@ class CoupledNetwork(BackBone):
 
         self.decoder_loss_fn = getattr(Losses, self.decoder_distance)()
 
-            
+        if self.aggregation == "none":
+            raise ValueError('Aggregation function must not be none for Coupled or Siamese networks')
+        aggregation_args = {}
+        self.aggregation_fn = aggregation_resolver(self.aggregation, **aggregation_args)
 
-    
+            
     def forward(self, batch, return_reg_loss=False):
         set1, set2 = batch
         batch_size = set1.batch_size
         v1, _ = self.encoder1(set1) # for now, ignore regularization for encoders/decoders
         v2, _ = self.encoder2(set2)
-        v1 = self.encoder_projection_1(v1.mean())
-        v2 = self.encoder_projection_2(v2.mean())
+
+        v1 = self.aggregation_fn(v1.data, ptr=v1.ptr)
+        v2 = self.aggregation_fn(v2.data, ptr=v2.ptr)
+
+        v1 = self.encoder_projection_1(v1)
+        v2 = self.encoder_projection_2(v2)
 
         predicted_distances = torch.linalg.vector_norm(v1 - v2, ord=self.p, dim=-1)
 

tests/test_models.py
@@ -1,5 +1,6 @@
 from typing import Set
 from torch.utils.data import DataLoader
+from torch import float32
 
 import lightning as pl
 
@@ -10,6 +11,9 @@ from nugets.tasks.dummy_tasks import SetIdentityTask, SingleLabelDummyTask
 from nugets.pipeline.configs import Config
 
 from pathlib import Path
+
+# TODO: Issue with mixed-precision training -- needed for fast attention.
+# TODO: Issue with num-workers, could have to do with debugger mode? 
 
 def test_transformer_with_labeled_set_task():
     """
@@ -41,14 +45,14 @@ def test_transformer_with_labeled_set_task():
             aggregation = 'mean' 
             )
     model = Model(backbone, task, batch_size=32, learning_rate=1e-3, debug_mode=True)
-    
+    model = model.to(dtype=float32)
     additional_options = dict()
     trainer = pl.Trainer(default_root_dir="workdir/", 
-                         max_epochs=1, 
+                         max_epochs=2, 
                          limit_train_batches=2,
                          log_every_n_steps=1,
                          gradient_clip_val=0, 
-                        #  precision="16-mixed",
+                         precision="16-mixed",
                          logger=False, 
                          use_distributed_sampler=False,
                          devices=1,
@@ -82,13 +86,14 @@ def test_sumformer_with_labeled_set_task():
         aggregation='sum'
     )
     model = Model(backbone, task, batch_size=32, learning_rate=1e-3, debug_mode=True)
+    model = model.to(dtype=float32)
     additional_options = dict()
     trainer = pl.Trainer(default_root_dir="workdir/", 
                          max_epochs=1, 
                          limit_train_batches=2,
                          log_every_n_steps=1,
                          gradient_clip_val=0, 
-                        #  precision="16-mixed",
+                         precision="16-mixed",
                          logger=False, 
                          use_distributed_sampler=False,
                          devices=1,
@@ -126,14 +131,13 @@ def test_transformer_with_dummy_set_to_set_task():
 
     model = Model(backbone, task, batch_size=32, learning_rate=1e-3, debug_mode=True)
     additional_options = dict()
-    # TODO: Issue with mixed-precision training -- needed for fast attention.
-    # TODO: Issue with num-workers, could have to do with debugger mode? 
+    model = model.to(dtype=float32)
     trainer = pl.Trainer(default_root_dir="workdir/", 
                          max_epochs=1, 
                          limit_train_batches=2,
                          log_every_n_steps=1,
-                         gradient_clip_val=0, 
-                        #  precision="16-mixed",
+                         gradient_clip_val=0.01, 
+                         precision="16-mixed",
                          logger=False, 
                          use_distributed_sampler=False,
                          devices=1,