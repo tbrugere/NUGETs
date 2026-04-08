5eaa009cae8ea3bc0e7204ec11c1be9a86a90263
sac003 <sac003@login1.vista.tacc.utexas.edu>
2026-04-08 17:11:36 -0500

    test script changes

--- testing.py
+++ testing.py
@@ -8,6 +8,7 @@ from nugets.datasets.datapoint_types import *
 from tqdm import tqdm
 import csv
 import argparse
+from nugets.pipeline.configs import Config
 
 def get_range_search_accuracy(model):
     return 
@@ -26,8 +27,8 @@ def get_mean_relative_error(model):
     test = model.test_dataloader()
     for batch in tqdm(test):
         pred = model(batch).detach().numpy()
-        distances = batch.distances.numpy()
-        avg_relative_error = (pred - distances)/(distances + 1e-5)
+        distances = batch.distance.numpy()
+        avg_relative_error = np.abs(pred - distances)/(distances + 1e-5)
     return np.mean(avg_relative_error), np.std(avg_relative_error), avg_relative_error
 
 
@@ -46,21 +47,27 @@ for i in range(len(yaml_files)):
     model = Model.from_config_file(Path(cfg_file))
     dirname = model.get_dirname()
     ckpt_dir = f'workdir/models/{dirname}'
-    if not Path(ckpt_dir).exists() or not any(Path(ckpt_dir).glob('*.pt')):
+    if not Path(ckpt_dir).exists() or not any(Path(ckpt_dir).glob('*.ckpt')) :
         print("Experiment not finished:", cfg_file)
         continue
-    latest_ckpt = max(Path(ckpt_dir).glob("*.pt"), key=lambda f: f.stat().st_mtime)
-    ckpt = torch.load(f'workdir/models/{dirname}/{latest_ckpt}')
+    print(ckpt_dir, '\n')
+    print('checkpoint in', dirname)
+    latest_ckpt = max(Path(ckpt_dir).glob("*.ckpt"), key=lambda f: f.stat().st_mtime)
+    ckpt = torch.load(f'{latest_ckpt}')
     model.load_state_dict(ckpt['state_dict'])
-    model.eval()
     model.batch_size = 100000
     all_models.append(model)
 
-with open(f'workdir/results/{args.output_file}.csv', newline='') as csvfile:
+cfg_pth = Path('config.yaml')
+Config.load(cfg_pth)
+config = Config.get()
+
+with open(f'workdir/results/{args.output_file}.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for i in range(len(yaml_files)):
         name = yaml_files[i].name[:-5]
         model = all_models[i]
-        avg, std, all = get_mean_relative_error(model)
+        
+        avg, std, all_err = get_mean_relative_error(model)
         print(name, avg, std)
-        writer.writerow([name, all])
+        writer.writerow([name, all_err])
